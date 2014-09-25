af27e1ac4ec2b572f8fab520bf364a2ff4ec7598
Andre de Oliveira <deoliveirambx@googlemail.com>
2014-09-25 03:37:05 -0300

    improves this testcode readability

--- tests/regress.c
+++ tests/regress.c
@@ -31,8 +31,6 @@
 #include "tools.h"
 
 #define SIZBUF 1024
-char	rawbuf[SIZBUF];
-char	encbuf[SIZBUF];
 rediscrypt_key_t key[KEY_SIZE] = { 
 	0xffaabbcc,
 	0xaabbccdd,
@@ -43,90 +41,115 @@ rediscrypt_key_t key[KEY_SIZE] = {
 void
 test_decrypt()
 {
+	char		rawbuf[SIZBUF];
+	size_t		rawbuflen;
+	char		decrypted_buf[SIZBUF];
+	u_int32_t	encrypted_buf[] = { 0xb4f8b91b, 0x051b3206, 0x29683e3a,
+			    0xa3690f16 };
+
 	fprintf(stderr, "==> begin test decrypt\n");
+
 	bzero(rawbuf, SIZBUF);
-	bzero(encbuf, SIZBUF);
+	bzero(decrypted_buf, SIZBUF);
+
 	strncpy(rawbuf, "test hello world", SIZBUF);
-	size_t buflen = cryptredis_align64(strlen(rawbuf));
-	u_int32_t encrybuf[] = {
-		0xb4f8b91b, 0x051b3206, 0x29683e3a, 0xa3690f16 };
+	rawbuflen = cryptredis_align64(strlen(rawbuf));
 
-	char decrybuf[SIZBUF];
-	bzero(decrybuf, SIZBUF);
+	cryptredis_decrypt(key, encrypted_buf, decrypted_buf,
+	    sizeof(encrypted_buf));
 
-//	decrypt_wrap(key,  (u_int32_t *)encrybuf, rawbuf, buflen);
-	cryptredis_decrypt(key, encrybuf, decrybuf, sizeof(encrybuf));
 	fprintf(stderr, "=> rawbuf: %s\n", rawbuf);
-	fprintf(stderr, "=> decrybuf: %s\n", decrybuf);
-	assert(strncmp(rawbuf, decrybuf, strlen(rawbuf)) == 0);
+	fprintf(stderr, "=> decrybuf: %s\n", decrypted_buf);
+
+	assert(strncmp(rawbuf, decrypted_buf, strlen(rawbuf)) == 0);
+
 	fprintf(stderr, "==> end test decrypt\n");
 }
 
 void
 test_encrypt()
 {
-	fprintf(stderr, "==> begin test make64align\n");
-	bzero(rawbuf, SIZBUF);
-	bzero(encbuf, SIZBUF);
-	strncpy(rawbuf, "test hello world", SIZBUF);
+	char		rawbuf[SIZBUF];
+	char		decrypted_buf[SIZBUF];
+	u_int32_t	encrypted_buf[] = { 0xb4f8b91b, 0x051b3206, 0x29683e3a,
+			    0xa3690f16 };
+	u_int32_t	dstencbuf[SIZBUF];
+	size_t		buflen;
 
-	size_t buflen = cryptredis_align64(strlen(rawbuf));
+	fprintf(stderr, "==> begin test make64align\n");
+
+	bzero(rawbuf, SIZBUF);
+	strncpy(rawbuf, "test hello world", SIZBUF);
+	buflen = cryptredis_align64(strlen(rawbuf));
 	assert(buflen == 16);
+
 	fprintf(stderr, "=> make64align(dstlen): %ld\n", buflen);
 	fprintf(stderr, "==> end test make64align\n");
-
 	fprintf(stderr, "==> begin test encrypt\n");
-	u_int32_t encrybuf[] = {
-		0xb4f8b91b, 0x051b3206, 0x29683e3a, 0xa3690f16 };
-	encrypt_wrap(key, rawbuf, (u_int32_t *)encbuf, buflen);
-	cryptredis_dumphex32("=> encbuf", encbuf, buflen);
-	assert(memcmp(encbuf, encrybuf, buflen) == 0);
+
+	encrypt_wrap(key, rawbuf, (u_int32_t *)dstencbuf, buflen);
+	cryptredis_dumphex32("=> dstencbuf", dstencbuf, buflen);
+	assert(memcmp(dstencbuf, encrypted_buf, buflen) == 0);
+
 	fprintf(stderr, "==> end test encrypt\n");
-
-
 }
 
 void
 test_encode()
 {
 	char		*encoded_src = "YClYLQ5zCd78M2urDXiRcw==";
-	u_int32_t	buf[] = { 0x2d582960, 0xde09730e, 0xab6b33fc, 0x7391780d };
-	size_t	   buflen = sizeof(buf);
+	u_int32_t	 buf[] = { 0x2d582960, 0xde09730e, 0xab6b33fc,
+			    0x7391780d };
 	char		*encoded_dst;
-
-	if ((encoded_dst = (char *)calloc(1, cryptredis_encsiz(buflen))) == NULL)
-		return;
+	size_t		 buflen;
 
 	fprintf(stderr, "==> begin test encode\n");
+
+	buflen = sizeof(buf);
+	if ((encoded_dst = (char *)calloc(1, cryptredis_encsiz(buflen))) ==
+	    NULL) return;
+
 	cryptredis_encode(encoded_dst, cryptredis_encsiz(buflen), buf, buflen);
+
 	fprintf(stderr, "=> encoded_src: %s\n", encoded_src);
 	fprintf(stderr, "=> encoded_dst: %s\n", encoded_dst);
 	fprintf(stderr, "=> strlen(encoded_src): %ld\n", strlen(encoded_src));
 	fprintf(stderr, "=> strlen(encoded_dst): %ld\n", strlen(encoded_dst));
+
 	assert(strlen(encoded_src) == strlen(encoded_dst));
 	assert(strncmp(encoded_src, encoded_dst, strlen(encoded_src)) == 0);
+
 	free(encoded_dst);
-	encoded_dst = NULL;
+
 	fprintf(stderr, "==> end test encode\n");
 }
 
 void
 test_decode()
 {
+	char		*encodeds = "YClYLQ5zCd78M2urDXiRcw==";
+	u_int32_t	 cryptbuf_src[] = { 0x2d582960, 0xde09730e, 0xab6b33fc,
+			    0x7391780d };
+	u_int32_t	 cryptbuf_dst[SIZBUF];
+	size_t		 buflen, len;
+
 	fprintf(stderr, "==> begin test decode\n");
-	u_int32_t decdbuf[SIZBUF];
-	u_int32_t encrybuf[] = { 0x2d582960, 0xde09730e, 0xab6b33fc, 0x7391780d };
-	char encds[] = "YClYLQ5zCd78M2urDXiRcw==";
-	size_t buflen = sizeof(encrybuf);
-	size_t len = cryptredis_decode(encds, decdbuf, SIZBUF);
-	fprintf(stderr, "=> encrybuf: x%08x x%08x x%08x x%08x\n", 
-			encrybuf[0], encrybuf[1], encrybuf[2], encrybuf[3]);
-	fprintf(stderr, "=> decdbuf:  x%08x x%08x x%08x x%08x\n",
-			decdbuf[0], decdbuf[1], decdbuf[2], decdbuf[3]);
+	fprintf(stderr, "=> cryptbuf_src: x%08x x%08x x%08x x%08x\n",
+	    cryptbuf_src[0], cryptbuf_src[1], cryptbuf_src[2],
+	    cryptbuf_src[3]);
+	fprintf(stderr, "=> cryptbuf_dst:  x%08x x%08x x%08x x%08x\n",
+	    cryptbuf_dst[0], cryptbuf_dst[1], cryptbuf_dst[2],
+	    cryptbuf_dst[3]);
+
+	buflen = sizeof(cryptbuf_src);
+	len = cryptredis_decode(encodeds, cryptbuf_dst, SIZBUF);
+
 	fprintf(stderr, "=> len: %ld\n", len);
 	fprintf(stderr, "=> buflen: %ld\n", buflen);
+
 	assert(buflen == len);
-	assert(memcmp(decdbuf, encrybuf, buflen) == 0);
+	assert(memcmp(cryptbuf_dst, cryptbuf_src, buflen) == 0);
+
 	fprintf(stderr, "==> end test decode\n");
 }
 
